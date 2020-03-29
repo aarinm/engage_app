19592695dba8801a28587694e2a3a5fd4787cafb
DayuenKim <47720554+DayuenKim@users.noreply.github.com>
2020-03-29 07:16:29 -0700

    Add files via upload

---
+++ Activity1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.Support.V7.App;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+
+
+namespace App3
+{
+    [Activity(Label = "Activity1")]
+    public class Activity1 : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            JSON json = new JSON();
+            string start=null, end=null;
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.exercises);
+            Button yog = FindViewById<Button>(Resource.Id.yoga);
+            yog.Click += delegate
+            {
+                if (start != null && end != null)
+                {
+                    json.Activity.addExercise("yoga", start, end);
+                }
+
+            };
+            Button cal = FindViewById<Button>(Resource.Id.calisthenics);
+            cal.Click += delegate
+            {
+                if (start != null && end != null)
+                {
+                    json.Activity.addExercise("Calisthenics", start, end);
+                }
+            };
+            Button cardio = FindViewById<Button>(Resource.Id.cardio);
+            cardio.Click += delegate
+            {
+                if (start != null && end != null)
+                {
+                    json.Activity.addExercise("cardio", start, end);
+                }
+            };
+
+            Button circuits = FindViewById<Button>(Resource.Id.circuits);
+            circuits.Click += delegate
+            {
+                if (start != null && end != null)
+                {
+                    json.Activity.addExercise("circuits", start, end);
+                }
+            };
+            Button weight = FindViewById<Button>(Resource.Id.weight);
+            weight.Click += delegate
+            {
+                if (start != null && end != null)
+                {
+                    json.Activity.addExercise("losing_weight", start, end);
+                }
+            };
+            Button muscle = FindViewById<Button>(Resource.Id.muscle);
+            muscle.Click += delegate
+            {
+                if (start != null && end != null)
+                {
+                    json.Activity.addExercise("gaining_muscle", start, end);
+                }
+            };
+            Button confirm = FindViewById<Button>(Resource.Id.confirm);
+            confirm.Click += async delegate
+            {
+                //GlobalVariable globalVariable = ((GlobalVariable)Android.App.Application.Context);
+
+                string message = "+13104876562";
+
+                json.Phone_number = message;
+
+                string output = JsonConvert.SerializeObject(json);
+                Console.WriteLine(output);
+                System.Diagnostics.Debug.Write(output);
+
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("http://826337de.ngrok.io/");
+
+
+                //var content = new StringContent(output);
+                var content = new StringContent(output, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("/sms", content);
+
+                // this result string should be something like: "{"token":"rgh2ghgdsfds"}"
+                var result = await response.Content.ReadAsStringAsync();
+                
+              
+                
+            };
+
+
+            EditText edittext = FindViewById<EditText>(Resource.Id.beg);
+            edittext.KeyPress += (object sender, View.KeyEventArgs e) =>
+            {
+                e.Handled = false;
+                if (e.Event.Action == KeyEventActions.Down && e.KeyCode == Keycode.Enter)
+                {
+                    start = edittext.Text;
+
+                    e.Handled = true;
+
+                }
+            };
+            EditText edittext2 = FindViewById<EditText>(Resource.Id.end);
+            edittext2.KeyPress += (object sender, View.KeyEventArgs e) =>
+            {
+                e.Handled = false;
+                if (e.Event.Action == KeyEventActions.Down && e.KeyCode == Keycode.Enter)
+                {
+                    end = edittext2.Text;
+
+                    e.Handled = true;
+
+                }
+            };
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,205 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Android.Views;
+using System.Net.Http;
+using System.Text;
+
+
+
+
+
+namespace App3
+{
+   
+    public class Exercise
+    {
+        public string name;
+        public string start_time;
+        public string end_time;
+    }
+    public class Art
+    {
+        public string name;
+        public string start_time;
+        public string end_time;
+    }
+    public class Music
+    {
+        public string name;
+        public string start_time;
+        public string end_time;
+    }
+
+    public class Language
+    {
+        public string name;
+        public string start_time;
+        public string end_time;
+    }
+
+    public class Cooking
+    {
+        public string name;
+        public string start_time;
+        public string end_time;
+    }
+    public static class Phonenumber
+    {
+        public static string phone_number;
+        public static void setnumber(string phone)
+        {
+            phone_number = phone;
+        }
+        public static string getnumber()
+        {
+            return phone_number;
+        }
+    }
+
+    public class Activity_
+    {
+        public List<Exercise> Exercise = new List<Exercise>();
+        public List<Music> Music=new List<Music>();
+        public List<Art> Art = new List<Art>();
+        public List<Language> Language = new List<Language>();
+        public List<Cooking> Cooking = new List<Cooking>();
+        
+       
+        public void addExercise(string name, string start_time, string end_time)
+        {
+            Exercise newExercise = new Exercise();
+            newExercise.name = name;
+            newExercise.start_time = start_time;
+            newExercise.end_time = end_time;
+            Exercise.Add(newExercise);
+        }
+        public void addMusic(string name, string start_time, string end_time)
+        {
+            Music newMusic = new Music();
+            newMusic.name = name;
+            newMusic.start_time = start_time;
+            newMusic.end_time = end_time;
+            Music.Add(newMusic);
+        }
+        public void addArt(string name, string start_time, string end_time)
+        {
+            Art newArt = new Art();
+            newArt.name = name;
+            newArt.start_time = start_time;
+            newArt.end_time = end_time;
+            Art.Add(newArt);
+        }
+        public void addLanguage(string name, string start_time, string end_time)
+        {
+            Language newLanguage = new Language();
+            newLanguage.name = name;
+            newLanguage.start_time = start_time;
+            newLanguage.end_time = end_time;
+            Language.Add(newLanguage);
+        }
+        public void addCooking(string name, string start_time, string end_time)
+        {
+            Cooking newCooking = new Cooking();
+            newCooking.name = name;
+            newCooking.start_time = start_time;
+            newCooking.end_time = end_time;
+            Cooking.Add(newCooking);
+        }
+    }
+    
+
+    public class JSON
+    {
+        public string Phone_number;
+        public Activity_ Activity = new Activity_();
+       
+    }
+
+
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+
+    
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            
+
+            JSON json = new JSON();
+            EditText edittext = FindViewById<EditText>(Resource.Id.editText);
+            edittext.KeyPress +=  (object sender, View.KeyEventArgs e) =>
+            {
+                e.Handled = false;
+                if (e.Event.Action == KeyEventActions.Down && e.KeyCode == Keycode.Enter)
+                {
+                    //json.Phone_number = "+"+ edittext.Text;
+
+                    
+
+
+                    e.Handled = true;
+
+                }
+                
+                
+            };
+            
+
+            
+          
+
+            Button button = FindViewById<Button>(Resource.Id.button);
+
+           
+            button.Click += delegate
+            {
+                var intent = new Intent(this, typeof(Activity1));
+                StartActivity(intent);
+            };
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+           
+            button1.Click += delegate
+            {
+                var intent = new Intent(this, typeof(Activity2));
+                StartActivity(intent);
+            };
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+          
+            button2.Click += delegate
+            {
+                var intent = new Intent(this, typeof(Activity3));
+                StartActivity(intent);
+            };
+
+            Button button3 = FindViewById<Button>(Resource.Id.button3);
+          
+            button3.Click += delegate
+            {
+                var intent = new Intent(this, typeof(Activity4));
+                StartActivity(intent);
+            };
+
+            Button button4 = FindViewById<Button>(Resource.Id.button4);
+            
+            button4.Click += delegate
+            {
+                var intent = new Intent(this, typeof(Activity5));
+                StartActivity(intent);
+            };
+           
+            
+        }
+    }
+}
